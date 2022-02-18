86d01e8f0145205e616c2ea2cbe6cba841bfe2c0
Drafti.sh <67260470+leakyboi@users.noreply.github.com>
2022-02-18 19:05:12 +0000

    women cant vote bro

--- program.py
+++ program.py
@@ -14,10 +14,13 @@ Age = int(input("Please enter your age: "))
 # Telephone Variable
 Telephone = str(input("Please enter your telephone number: "))
 
+#this person a woman?!?!?!?!?!?1/1/1/?!?!?1/1/11/1/?!?!
+isWoman = bool(input("Yo you a woman?: "))
+
 # Vote Variable
 def Vote1():
     Eligibility = False
-if Age >= 18:
+if Age >= 18 and !isWoman:
     print('Checking the UK Voting Database..')
     # Program will sleep for 5 seconds
     time.sleep(5)
@@ -46,7 +49,7 @@ else:
     print(' Age: ', Age)
     print(' Telephone: ' + Telephone)
     print(' ')
-    print('   You are NOT eligible to Vote!')
+    print('   You are NOT eligible to Vote because you are a woman!')
     print('-------------------------')
 
 # Main Program
